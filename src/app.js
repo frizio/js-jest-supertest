b321fe52e91712fcee0ece44779859ad4a91ebcc
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-02-23 11:47:26 +0100

    Define /tasks route and test it

--- src/app.js
+++ src/app.js
@@ -2,10 +2,18 @@ import express from "express";
 
 const app = express();
 
+const tasks = [];
+
+
 // Routes
 
 app.get("/ping", (req, res) => {
   res.send("Pong");
 });
+
+app.get("/tasks", (req, res) => {
+  res.status(200).json(tasks);
+});
+
 
 export default app;
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -2,9 +2,17 @@ import request from "supertest";
 import app from "../src/app";
 
 describe("GET /ping", () => {
-
   test("should respond with a 200 status code", async () => {
     const response = await request(app).get("/ping").send();
+    expect(response.statusCode).toBe(200);
+  });
+})
+
+
+describe("GET /tasks", () => {
+
+  test("should respond with a 200 status code", async () => {
+    const response = await request(app).get("/tasks").send();
     expect(response.statusCode).toBe(200);
   });
 
