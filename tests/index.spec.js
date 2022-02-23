bd4241d77fa8d4aff367b8030de3b87a4dd3c71d
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-02-23 10:46:20 +0100

    First test passed

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,1 +1,11 @@
-console.log('Test');
+import request from "supertest";
+import app from "../src/app";
+
+describe("GET /ping", () => {
+
+  test("should respond with a 200 status code", async () => {
+    const response = await request(app).get("/ping").send();
+    expect(response.statusCode).toBe(200);
+  });
+
+})
